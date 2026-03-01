af7ddef77223798788d2eba975e275fb389ae7e4
prath <prathammewara7773@gmail.com>
2026-03-01 23:33:39 +0530

    feat: implemented basic metrics dashboard using Streamlit

---
+++ app/app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+from click import option
+from streamlit.elements.widgets.radio import T
+
+from db_function import (
+connect_to_db,
+get_basic_info
+)
+
+#sidebar
+st.sidebar.title("Inventory Management Dashboard")
+option = st.sidebar.radio("Select Option:",["Basic Information" , "Operational Tasks"])  #button click kar tha hai
+
+
+#main space
+st.title("Inventory and Supply chain Dashboard")
+db = connect_to_db()
+cursor = db.cursor(dictionary=True)
+
+
+
+# -----------------------------------------------BASIC INFORMATION PAGE------------------------------------------------
+if option == "Basic Information":
+    st.header("Basic Metrics")
+
+    #get basic information from data base
+    basic_info = get_basic_info(cursor)
+
+    cols = st.columns(3)
+    keys = list(basic_info.keys())
+
+    for i in range (3):
+        cols[i].metric(label=keys[i],value=basic_info[keys[i]])
+
+
+    cols = st.columns(3)
+    for i in range (3,6):
+        cols[i-3].metric(label=keys[i],value=basic_info[keys[i]])
+
+
+    st.divider()
--- database/quries.sql
+++ database/quries.sql
@@ -6,11 +6,18 @@
 
 USE inventory_management;
 
+-- ============================================
+-- BASIC DATA VIEWING
+-- ============================================
 select * from products;
 select * from reorders;
 select * from shipments;
 select * from stock_entries;
 select* from suppliers;
+
+-- ============================================
+-- BUSINESS METRICS
+-- ============================================
 
 -- 1)Total Suppliers -:
 SELECT 
@@ -33,6 +40,50 @@ FROM
     products;
     
 
--- 4)Total sales value made in last 3 month -:
+-- 4)Total sales value made in last 3 month (quantiy * price) -:
+SELECT 
+    ROUND(SUM(ABS(se.change_quantity) * p.price),
+            2) AS total_sales_value_last_3_month
+FROM
+    stock_entries AS se
+        JOIN
+    products AS p ON p.product_id = se.product_id
+WHERE
+    se.change_type = 'Sale'
+        AND se.entry_date >= (SELECT 
+            DATE_SUB(MAX(entry_date),
+                    INTERVAL 3 MONTH)
+        FROM
+            stock_entries);
+            
 
- 
+-- 5)Total restock value made in last 3 month (quantiy * price) -:
+SELECT 
+    ROUND(SUM(ABS(se.change_quantity) * p.price),
+            2) AS total_sales_value_last_3_month
+FROM
+    stock_entries AS se
+        JOIN
+    products AS p ON p.product_id = se.product_id
+WHERE
+    se.change_type = 'Restock'
+        AND se.entry_date >= (SELECT 
+            DATE_SUB(MAX(entry_date),
+                    INTERVAL 3 MONTH)
+        FROM
+            stock_entries);
+            
+            
+-- 6)Below reorder & NO pending reorders
+SELECT 
+    COUNT(*) AS products_needing_reorder
+FROM
+    products AS p
+WHERE
+    p.stock_quantity < p.reorder_level
+        AND product_id NOT IN (SELECT DISTINCT
+            (product_id)
+        FROM
+            reorders
+        WHERE
+            status = 'Pending');
