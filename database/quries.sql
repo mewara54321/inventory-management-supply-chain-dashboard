c49741030ab86706ad5dade7935be1294a8068c8
prath <prathammewara7773@gmail.com>
2026-03-02 21:25:04 +0530

    feat: added product inventory history view for unified shipment and stock tracking

--- database/quries.sql
+++ database/quries.sql
@@ -172,3 +172,35 @@ END $$
 DELIMITER ;
 
 
+-- 11)Product history ,[finding shipment , sales ,purchase]
+CREATE OR REPLACE VIEW product_inventory_history AS
+    SELECT 
+        pih.product_id,
+        pih.record_type,
+        pih.record_date,
+        pih.quantity,
+        pih.change_type,
+        pr.supplier_id
+    FROM
+        (SELECT 
+                 product_id,
+                'Shipment' AS record_type,
+                shipment_date AS record_date,
+                quantity_received AS Quantity,
+                NULL change_type
+        FROM
+            shipments UNION ALL 
+            SELECT 
+                 product_id,
+                'Shipment' AS record_type,
+                entry_date AS record_date,
+                change_quantity AS Quantity,
+                change_type
+        FROM
+            stock_entries) pih
+            JOIN
+        products pr ON pr.product_id = pih.product_id;
+    
+
+
+
